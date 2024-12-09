472ed5752343a42a7fbcfad00898351526c300b0
mc-cat-tty <44820563+mc-cat-tty@users.noreply.github.com>
2024-12-09 10:41:01 +0100

    Finish script

--- profiling_utils/comparison_cuda.py
+++ profiling_utils/comparison_cuda.py
@@ -20,7 +20,7 @@ for size in DF["Dataset Size"].unique():
   baseline = DF[DF["Dataset Size"] == size][DF["Optimization"] == "Baseline"]["Time"].item()
   SPEEDUPS.append([baseline/time for time in DF[DF["Dataset Size"] == size]["Time"]])
 
-def main():
+def global_comparison():
   sb.set_theme(style="whitegrid")
   g = sb.catplot(
     data=DF,
@@ -41,6 +41,46 @@ def main():
   
   pl.yscale("log")
   pl.show()
+
+def large_dataset_comparison():
+  sb.set_theme(style="whitegrid")
+  g = sb.catplot(
+    data=DF[DF["Dataset Size"] == "Large"],
+    kind="bar",
+    x="Optimization",
+    y="Time",
+    hue="Dataset Size",
+    palette="dark",
+    alpha=.7,
+  )
+  
+  g.despine(left=True)
+  g.set_axis_labels("", "Time [s]")
+  g.legend.set_title("")
+  g.ax.grid(True, which="minor")
+  g.ax.bar_label(g.ax.containers[0], labels=[f"x{speedup:.1f}" for speedup in SPEEDUPS[-2]])
+  
+  # DF2 = pd.concat([
+  #   DF[DF["Dataset Size"] == "Large"],
+  #   pd.DataFrame({"Speedup": [s for s in SPEEDUPS[-2]], "Optimization": ["Baseline", "OMP", "CUDA"]})
+  # ])
+
+  # h = sb.lineplot(
+  #   data=DF2,
+  #   x="Optimization",
+  #   y="Speedup",
+  #   palette="dark",
+  #   alpha=.7,
+  #   legend=False,
+  # )
+  # h.set_ylim((min(SPEEDUPS[-2]), max(SPEEDUPS[-2])))
+
+  pl.yscale("linear")
+  pl.show()
+
+def main():
+  global_comparison()
+  large_dataset_comparison()
 
 if __name__ == "__main__":
   main()
